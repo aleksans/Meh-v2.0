e86995a2f00e86b4d79c58e26c5e396a9bfbd54e
aleksans <sergii.aleksandrov@gmail.com>
2016-04-17 22:44:41 +0200

    added basic angularjs implementation

---
+++ src/Meh/wwwroot/scripts/app.js
@@ -0,0 +1,8 @@
+﻿angular.module('app', ["ngRoute", "ngResource"]);
+
+angular.module('app').config(function ($routeProvider) {
+    $routeProvider.when('/', { templateUrl: 'views/articles.html', controller: 'ArticleController' });
+    $routeProvider.when('/article', { templateUrl: 'views/articles.html', controller: 'ArticleController' });
+    $routeProvider.otherwise({redirectTo: '/article'});
+});
+
---
+++ src/Meh/wwwroot/scripts/factories/articlesResource.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+angular.module('app')
+    .factory('articlesResource', function ($resource) {
+        return $resource('api/article/:id', {},
+        {
+            query: { method: "GET", isArray: true }
+        });
+    });
